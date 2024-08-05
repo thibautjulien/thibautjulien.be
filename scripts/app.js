d4436a1bcfa415cea566c2eeeddaaf229c878015
Howlnes/Thibaut <thbtjulien21@gmail.com>
2024-08-05 15:53:18 +0200

    Commit_05.08-MediasQueries+Modifications

--- scripts/app.js
+++ scripts/app.js
@@ -47,3 +47,20 @@ document.addEventListener("DOMContentLoaded", function() {
         typeWriter();
     });
 });
+
+/* Carousel */
+
+document.addEventListener("DOMContentLoaded", function() {
+    const carouselTrack = document.querySelector('.carousel-track');
+    const images = carouselTrack.querySelectorAll('img');
+    const totalImages = images.length;
+    let currentIndex = 0;
+
+    function scrollCarousel() {
+        currentIndex = (currentIndex + 1) % totalImages;
+        const translateX = -currentIndex * 100;
+        carouselTrack.style.transform = `translateX(${translateX}%)`;
+    }
+
+    setInterval(scrollCarousel, 6000);
+});
